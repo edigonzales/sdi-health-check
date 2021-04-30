bcefcc064acdfd94a4915876ff029f0d1e335431
Stefan Ziegler <edi.gonzales@gmail.com>
2021-04-30 19:41:45 +0200

    initial commit

---
+++ src/main/java/ch/so/agi/healthcheck/SdiHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.healthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiHealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiHealthCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/so/agi/healthcheck/SdiHealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.healthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiHealthCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
