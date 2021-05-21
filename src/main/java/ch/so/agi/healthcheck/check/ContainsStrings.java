1f11f518d6f825101adf8fc13b192d366e1f8216
edigonzales <edi.gonzales@gmail.com>
2021-05-21 15:32:37 +0200

    add (not)contains strings

--- src/main/java/ch/so/agi/healthcheck/check/ContainsStrings.java
+++ src/main/java/ch/so/agi/healthcheck/check/ContainsStrings.java
@@ -1,6 +1,16 @@
 package ch.so.agi.healthcheck.check;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ch.so.agi.healthcheck.model.CheckVarsDTO;
 import ch.so.agi.healthcheck.probe.ParamDefinition;
@@ -22,7 +32,23 @@ public class ContainsStrings extends Check {
     @Override
     public void perform(CheckVarsDTO checkVars) throws IOException {
         log.info("Performing: " + this.getClass().getCanonicalName());
-
+        
+        InputStream is = (InputStream) this.probe.getResponse().body();
+        String responseText = new BufferedReader(
+                new InputStreamReader(is, StandardCharsets.UTF_8))
+                  .lines()
+                  .collect(Collectors.joining("\n"));
+        
+        Map<String, Object> paramsMap = null;
+        paramsMap = new ObjectMapper().readValue(checkVars.getParameters(), HashMap.class);
+        strings = (String) paramsMap.get("strings");
+        
+        this.setResult(true, "OK");        
+        Arrays.asList(strings.split(",")).forEach(s -> {
+            if (!responseText.contains(s)) {
+                this.setResult(false, s+ " not in response text");
+                return;
+            }
+        });
     }
-
 }
--- src/main/java/ch/so/agi/healthcheck/check/NotContainsStrings.java
+++ src/main/java/ch/so/agi/healthcheck/check/NotContainsStrings.java
@@ -1,10 +1,23 @@
 package ch.so.agi.healthcheck.check;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ch.so.agi.healthcheck.model.CheckVarsDTO;
+import ch.so.agi.healthcheck.probe.ParamDefinition;
 
 public class NotContainsStrings extends Check {
+    @ParamDefinition(name = "strings", description = "The string text(s) that should be contained in response (comma-separated)")
+    private String strings;
 
     @Override
     public String getName() {
@@ -20,6 +33,23 @@ public class NotContainsStrings extends Check {
     public void perform(CheckVarsDTO checkVars) throws IOException {
         log.info("Performing: " + this.getClass().getCanonicalName());
         
+        InputStream is = (InputStream) this.probe.getResponse().body();
+        String responseText = new BufferedReader(
+                new InputStreamReader(is, StandardCharsets.UTF_8))
+                  .lines()
+                  .collect(Collectors.joining("\n"));
+        
+        Map<String, Object> paramsMap = null;
+        paramsMap = new ObjectMapper().readValue(checkVars.getParameters(), HashMap.class);
+        strings = (String) paramsMap.get("strings");
+
+        this.setResult(true, "OK");        
+        Arrays.asList(strings.split(",")).forEach(s -> {
+            if (responseText.contains(s)) {
+                this.setResult(false, s+ " in response text");
+                return;
+            }
+        });
     }
 
 }
